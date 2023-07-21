b4ef974b64656e64ba93f896f3c7eab2b92e9bfc
Raul Villarreal <raulvilla0797@gmail.com>
2023-07-21 16:38:02 -0600

    Initial Commit

---
+++ Lunchi/LunchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchiApp.swift
+//  Lunchi
+//
+//  Created by Raul Villarreal on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LunchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
